71859ac6d1609cc2d4e1b228d565f20552970588
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2021-02-07 21:06:08 +0200

    Added jwt login frontend, user actions, like, edit, delete, token expiry

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
 import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => {
     return (
         <Grid item xs={12} sm={half ? 6 : 12}>
@@ -19,7 +19,7 @@ const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPas
                     endAdornment: (
                         <InputAdornment position="end">
                             <IconButton onClick={handleShowPassword}>
-                                {type === "password" ? <Visibility/> : <VisibilityOff/>}
+                                {type === "password" ? <Visibility /> : <VisibilityOff />}
                             </IconButton>
                         </InputAdornment>
                     )
