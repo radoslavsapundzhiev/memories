3462a62cd9b35f4203857d0a81da8221fcf21832
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2021-02-08 12:29:52 +0200

    Added Procfile

--- server/index.js
+++ server/index.js
@@ -16,8 +16,12 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API');
+});
+
 const CONNECTION_URL = 'mongodb+srv://radoslav:radoslav.4361@cluster0.e6zoh.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
